aab7066d5d053aaba29d1ff833317cf11d596168
Anderson Vieira <anderson.vieira@unidas.com.br>
2021-05-17 19:28:44 -0300

    Added assets and data

---
+++ touchdown/Utility/Constant.swift
@@ -0,0 +1,30 @@
+//
+//  Constant.swift
+//  touchdown
+//
+//  Created by Anderson Simões Vieira on 17/05/21.
+//
+
+import SwiftUI
+
+
+// DATA
+
+// COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
+
---
+++ touchdown/View/Home/FooterView.swift
@@ -0,0 +1,42 @@
+//
+//  FooterView.swift
+//  touchdown
+//
+//  Created by Anderson Simões Vieira on 17/05/21.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, lightweight and durable football helmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright © Robert Petras\nAll right reserved")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(1)
+            
+        } //: VSTACK
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
