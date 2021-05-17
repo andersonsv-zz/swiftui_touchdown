f59560e2a24ed6e5b5c680a765ce9f2ba1f193bf
Anderson Vieira <anderson.vieira@unidas.com.br>
2021-05-17 16:13:04 -0300

    First commit

---
+++ touchdown/touchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchdownApp.swift
+//  touchdown
+//
+//  Created by Anderson Simões Vieira on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct touchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
