00e2606db50bf7b7536235f951a507836c32bbda
Anderson Vieira <anderson.vieira@unidas.com.br>
2021-06-13 17:17:48 -0300

    Add brand view

---
+++ touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  touchdown
+//
+//  Created by Anderson Simões Vieira on 13/06/21.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
